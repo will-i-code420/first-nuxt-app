f5ffaa073af99226d05cd3ea83af5bf6a8b35a6d
Will Aronow <waronow123@gmail.com>
2022-07-27 11:43:02 -0700

    created routing links and notes to their use

--- pages/users/index.vue
+++ pages/users/index.vue
@@ -1,3 +1,23 @@
 <template>
-  <h1>User Page</h1>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
+      <input type="text" v-model="userId" />
+      <v-btn color="primary" @click="loadUser">Load User</v-btn>
+    </v-col>
+  </v-row>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      userId: "",
+    };
+  },
+  methods: {
+    loadUser() {
+      this.$router.push(`/users/${this.userId}`);
+    },
+  },
+};
+</script>
