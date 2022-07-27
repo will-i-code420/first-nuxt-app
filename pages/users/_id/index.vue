cf5f1b79894f55c0ced0823e0be0ad507e52b34c
Will Aronow <waronow123@gmail.com>
2022-07-27 13:01:59 -0700

    made users_id route validate userId is acutally a number

--- pages/users/_id/index.vue
+++ pages/users/_id/index.vue
@@ -1,3 +1,12 @@
 <template>
   <h1>User ID: {{ $route.params.id }}</h1>
 </template>
+
+<script>
+export default {
+  // nuxt executes prior to loading route
+  validate(data) {
+    return /^\d+$/.test(data.params.id);
+  },
+};
+</script>
