b906d67eec18b393fd1c907f325328abf55ea04e
Will Aronow <waronow123@gmail.com>
2022-07-27 13:08:18 -0700

    created nested route to always display userID search box

---
+++ pages/users.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
+      <input type="text" v-model="userId" />
+      <v-btn color="primary" @click="loadUser">Load User</v-btn>
+      <nuxt-child />
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      userId: "",
+    };
+  },
+  methods: {
+    loadUser() {
+      this.$router.push(`/users/${this.userId}`);
+    },
+  },
+};
+</script>
--- pages/users/index.vue
+++ pages/users/index.vue
@@ -1,23 +1,4 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <input type="text" v-model="userId" />
-      <v-btn color="primary" @click="loadUser">Load User</v-btn>
-    </v-col>
-  </v-row>
+  <p>Input User ID to get more info</p>
 </template>
 
-<script>
-export default {
-  data() {
-    return {
-      userId: "",
-    };
-  },
-  methods: {
-    loadUser() {
-      this.$router.push(`/users/${this.userId}`);
-    },
-  },
-};
-</script>
