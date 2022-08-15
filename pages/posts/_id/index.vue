927f33c6d65f6d681308bb96af53a9a7a90a982e
Will Aronow <waronow123@gmail.com>
2022-08-15 13:38:39 -0700

    set up basic single post page

--- pages/posts/_id/index.vue
+++ pages/posts/_id/index.vue
@@ -2,11 +2,13 @@
   <v-row justify="center" align="center">
     <v-col cols="16" sm="12" md="10">
       <v-card class="logo py-4 d-flex justify-center">
-
+        <v-img class="align-end" :src="loadedPost.img">
+          <v-card-title class="headline justify-center">{{loadedPost.title}}</v-card-title>
+        </v-img>
       </v-card>
       <v-card>
-        <v-card-title></v-card-title>
-        <v-card-text></v-card-text>
+        <v-card-subtitle>{{loadedPost.author}} - Date: {{new Date()}}</v-card-subtitle>
+        <v-card-text>{{loadedPost.content}}</v-card-text>
         <v-card-actions>
         </v-card-actions>
       </v-card>
@@ -16,7 +18,16 @@
 
 <script>
 export default {
-  name: "SinglePostPage"
+  name: "SinglePostPage",
+  asyncData(context,cb) {
+    setTimeout(() => {
+      cb(null,{
+        loadedPost: {
+          id: 1, title: 'New Vue News', author: 'Bob Dole', content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', img: 'https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg'
+        }
+      })
+    }, 1000)
+  }
 }
 </script>
 
